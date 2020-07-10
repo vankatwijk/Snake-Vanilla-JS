bda150453d263904deaaa690f50d331cc9aa16bb
vankatwijk <hendrikus.hpvk@gmail.com>
2020-07-10 16:55:28 +0200

    keyborad input setup

--- snake.js
+++ snake.js
@@ -1,15 +1,17 @@
+import { getInputDirection } from "./input.js";
 
 export const SNAKE_SPEED = 2 //updates per second
 const snakeBody = [
-    {x:10,y:11},
-    {x:11,y:11},
-    {x:12,y:11}
+    {x:11,y:11}
 ]
 
 export function update(){
+    const inputDirection = getInputDirection()
     for(let i = snakeBody.length - 2; i>=0;i--){
         snakeBody[i +1] = { ...snakeBody[i]}
     }
+    snakeBody[0].x +=inputDirection.x
+    snakeBody[0].y +=inputDirection.y
 
     // snakeBody[0].x +=1 // hard coded movements
     // snakeBody[0].y +=0 // hard coded movements
