38e17a3b96e4789fa264f8bfb37e0c09ccc8fe0e
vankatwijk <hendrikus.hpvk@gmail.com>
2020-07-10 16:40:07 +0200

    init snake movement

--- snake.js
+++ snake.js
@@ -1,9 +1,18 @@
 
 export const SNAKE_SPEED = 2 //updates per second
-const snakeBody = [{x:11,y:11}]
+const snakeBody = [
+    {x:10,y:11},
+    {x:11,y:11},
+    {x:12,y:11}
+]
 
 export function update(){
+    for(let i = snakeBody.length - 2; i>=0;i--){
+        snakeBody[i +1] = { ...snakeBody[i]}
+    }
 
+    snakeBody[0].x +=1
+    snakeBody[0].y +=0
 }
 
 export function draw(gameBoard){
